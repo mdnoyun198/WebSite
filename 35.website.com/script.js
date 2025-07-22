5cc7145bf7c9802d67fa004660d2ae0897d52dd4
Md Noyun <mdnoyun198@gmail.com>
2025-07-22 16:55:54 +0600

    Add files via upload

---
+++ 35.website.com/script.js
@@ -0,0 +1,19 @@
+alert("hello")
+console.log("hello")
+ var a = prompt("enter your number")
+
+ console.log("ypur number is" + a)
+
+
+
+ var isture = confirm("con you blust your computer")
+
+ if(isture){
+
+console.log("your computer is blust")
+
+ }
+
+ else{
+    console.log("your computer not blost")
+ }
